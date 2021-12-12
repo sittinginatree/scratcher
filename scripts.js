f40bd2d4d8bff891570908edff4be41f5089daa9
sittinginatree <67747971+sittinginatree@users.noreply.github.com>
2021-12-12 16:38:16 +0000

    added gift info

--- scripts.js
+++ scripts.js
@@ -8,14 +8,9 @@ if (brushRadius < 50) { brushRadius = 50 }
 img.onload = function(){  
 	bridgeCanvas.drawImage(img, 0, 0, bridge.width, bridge.height);
 }
-img.loc = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/';
-img.filename = 'calgary-bridge-2013.jpg';
-if (window.devicePixelRatio >= 2) {
-	var nameParts = img.filename.split('.');
-	img.src = img.loc + nameParts[0]+"-2x"+"."+nameParts[1];
-} else {
-	img.src = img.loc + img.filename;
-}
+
+img.src = "chrisbert.jpg"
+
 
 function detectLeftButton(event) {
     if ('buttons' in event) {
